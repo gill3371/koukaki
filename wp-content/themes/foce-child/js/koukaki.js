2a1e41b0cdf7d4746a7996cce6eaac00cb6d159e
gill3371 <gilles.bailly@agencealchimie.fr>
2023-08-14 09:38:29 +0200

    footer

--- wp-content/themes/foce-child/js/koukaki.js
+++ wp-content/themes/foce-child/js/koukaki.js
@@ -1,32 +1,47 @@
-const observerBanner = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add('animDebutDown');
-            return;
-        }
-        // entry.target.classList.remove('animDebutDown');
-    })
-})
-observerBanner.observe(document.querySelector(".banner"));
+// const observerBanner = new IntersectionObserver(entries => {
+//     entries.forEach(entry => {
+//         if (entry.isIntersecting) {
+//             entry.target.classList.add('animDebutDown');
+//             return;
+//         }
+//         // entry.target.classList.remove('animDebutDown');
+//     })
+// })
+// observerBanner.observe(document.querySelector(".banner"));
 
-const observerMain = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add('animDebut');
-            return;
-        }
-        // entry.target.classList.remove('animDebut');
-    })
-})
-observerMain.observe(document.querySelector(".story"));
+// const observerMain = new IntersectionObserver(entries => {
+//     entries.forEach(entry => {
+//         if (entry.isIntersecting) {
+//             entry.target.classList.add('animDebut');
+//             return;
+//         }
+//         // entry.target.classList.remove('animDebut');
+//     })
+// })
+// observerMain.observe(document.querySelector(".story"));
 
-const observerStudio = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add('animDebut');
-            return;
-        }
-        // entry.target.classList.remove('animDebut');
-    })
-})
-observerStudio.observe(document.querySelector("#studio"));
+// const observerStudio = new IntersectionObserver(entries => {
+//     entries.forEach(entry => {
+//         if (entry.isIntersecting) {
+//             entry.target.classList.add('animDebut');
+//             return;
+//         }
+//         // entry.target.classList.remove('animDebut');
+//     })
+// })
+// observerStudio.observe(document.querySelector("#studio"));
+
+let sectionStudio = document.querySelector("#story");
+let rectStudio = sectionStudio.getBoundingClientRect();
+let posXsectionStudio = rectStudio.right;
+let posYsectionStudio = rectStudio.top;
+let windowHeight = window.innerHeight;
+let windowScroll = window.scrollY;
+
+console.log("top= ",posYsectionStudio);
+console.log("hauteur fenêtre= ",windowHeight);
+console.log("position fenêtre= ",windowScroll);
+
+// while (posYsectionStudio < 3000) {
+//     console.log(posXsectionStudio,posYsectionStudio);
+// }
---
+++ wp-content/themes/foce-child/nomination.php
@@ -0,0 +1,6 @@
+<section>
+    <div id="nomination">
+    <h3>Fleurs d’oranger & chats errants <br>nominé aux Oscars Short<br>Animated de 2022 !</h3>
+    <img src="<?php echo get_stylesheet_directory_uri() . '/asset/images/oscars.svg' ?>" alt="Logo des Oscars - Short Film Animated">
+    </div>
+</section>
