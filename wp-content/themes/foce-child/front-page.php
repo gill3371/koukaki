8bfc35d121b9e6248d1206183b0460625eb21ba9
gill3371 <gilles.bailly@agencealchimie.fr>
2023-08-20 10:32:30 +0200

    floating logo

--- wp-content/themes/foce-child/front-page.php
+++ wp-content/themes/foce-child/front-page.php
@@ -3,22 +3,22 @@
 get_header();
 ?>
 
-    <main id="primary" class="site-main">
-        <section class="banner">
-            <div class="bannerDiv">
-                <video muted autoplay loop id="myVideo" poster="<?php echo get_template_directory_uri() . '/assets/images/banner.png'?>">
-                    <source src="<?php echo get_stylesheet_directory_uri() . '/asset/video/Studio-Koukaki-vidéo.mp4'?>" type="video/mp4">
-                    Your browser does not support HTML5 video.
-                </video>
-                <img src="<?php echo get_template_directory_uri() . '/assets/images/logo.png'; ?>" alt="logo Fleurs d'oranger & chats errants"
-                data-bottom-top="transform:translate3d(0, -100px, 0)"
-                data-top-bottom="transform:translate3d(0, 100px, 0)"
-                >
-            </div>
-        </section>
-        <section id="story" class="story">
-            <div class="storyDiv">
-            <h2>L'histoire</h2>
+<main id="primary" class="site-main">
+    <section class="banner">
+        <div class="bannerDiv">
+            <video muted autoplay loop id="myVideo" poster="<?php echo get_template_directory_uri() . '/assets/images/banner.png' ?>">
+                <source src="<?php echo get_stylesheet_directory_uri() . '/asset/video/Studio-Koukaki-vidéo.mp4' ?>" type="video/mp4">
+                Your browser does not support HTML5 video.
+            </video>
+            <img class="animFloating" src="<?php echo get_template_directory_uri() . '/assets/images/logo.png'; ?>" alt="logo Fleurs d'oranger & chats errants"
+            data-bottom-top="transform:translate3d(0, -50px, 0)"
+            data-top-bottom="transform:translate3d(0, 300px, 0)"
+            >
+        </div>
+    </section>
+    <section id="story" class="story">
+        <div class="storyDiv">
+            <h2><span class="storyTitle">L'histoire</span></h2>
             <article id="" class="story__article">
                 <p><?php echo get_theme_mod('story'); ?></p>
             </article>
@@ -38,21 +38,21 @@ get_header();
                     <?php
                     $main_character = $characters_query->posts[0];
                     echo '<figure>';
-                    echo get_the_post_thumbnail( $main_character->ID, 'full' );
-                    echo '<figcaption>'. $main_character->post_title . '</figcaption>';
+                    echo get_the_post_thumbnail($main_character->ID, 'full');
+                    echo '<figcaption>' . $main_character->post_title . '</figcaption>';
                     echo '</figure>';
                     $characters_query->next_post();
                     ?>
                 </div>
                 <div class="other-characters">
                     <?php
-                    while ( $characters_query->have_posts() ) {
+                    while ($characters_query->have_posts()) {
                         $characters_query->the_post();
                         echo '<figure>';
-                        echo get_the_post_thumbnail( get_the_ID(), 'full' );
+                        echo get_the_post_thumbnail(get_the_ID(), 'full');
                         echo '<figcaption>';
                         the_title();
-                        echo'</figcaption>';
+                        echo '</figcaption>';
                         echo '</figure>';
                     }
                     ?>
@@ -65,20 +65,20 @@ get_header();
                 </div>
 
             </article>
-            </div>
-        </section>
+        </div>
+    </section>
 
 
-        <section id="studio" class="">
-            <div class="studioDiv">
-            <h2><span class="titre">Studio</span> <span class="titreb">Koukaki</span></h2>
+    <section id="studio" class="">
+        <div class="studioDiv">
+            <h2><span class="title_A">Studio</span> <span class="title_B">Koukaki</span></h2>
             <div class="textStudio">
                 <p>Acteur majeur de l’animation, Koukaki est un studio intégré fondé en 2012 qui créé, produit et distribue des programmes originaux dans plus de 190 pays pour les enfants et les adultes. Nous avons deux sections en activité : le long métrage et le court métrage. Nous développons des films fantastiques, principalement autour de la culture de notre pays natal, le Japon.</p>
                 <p>Avec une créativité et une capacité d’innovation mondialement reconnues, une expertise éditoriale et commerciale à la pointe de son industrie, le Studio Koukaki se positionne comme un acteur incontournable dans un marché en forte croissance. Koukaki construit chaque année de véritables succès et capitalise sur de puissantes marques historiques. Cette année, il vous présente “Fleurs d’oranger et chats errants”.</p>
             </div>
-            </div>
-            </section>
-    </main><!-- #main -->
+        </div>
+    </section>
+</main><!-- #main -->
 
 
 <?php
--- wp-content/themes/foce-child/js/koukaki.js
+++ wp-content/themes/foce-child/js/koukaki.js
@@ -1,57 +1,11 @@
-// const observerBanner = new IntersectionObserver(entries => {
-//     entries.forEach(entry => {
-//         if (entry.isIntersecting) {
-//             entry.target.classList.add('animDebutDown');
-//             return;
-//         }
-//         // entry.target.classList.remove('animDebutDown');
-//     })
-// })
-// observerBanner.observe(document.querySelector(".banner"));
-
-// const observerMain = new IntersectionObserver(entries => {
-//     entries.forEach(entry => {
-//         if (entry.isIntersecting) {
-//             entry.target.classList.add('animDebut');
-//             return;
-//         }
-//         // entry.target.classList.remove('animDebut');
-//     })
-// })
-// observerMain.observe(document.querySelector(".story"));
-
-// const observerStudio = new IntersectionObserver(entries => {
-//     entries.forEach(entry => {
-//         if (entry.isIntersecting) {
-//             entry.target.classList.add('animDebut');
-//             return;
-//         }
-//         // entry.target.classList.remove('animDebut');
-//     })
-// })
-// observerStudio.observe(document.querySelector("#studio"));
-
-
-// function fadeinAnim ($arg,$anim) {
-    //     let rectArg = $arg.getBoundingClientRect();
-    //     let posY = rectArg.top;
-    //     let bottomElement = rectArg.bottom;
-    //     let windowHeight = window.innerHeight;
-    //     let windowScroll = window.scrollY;
-    //     console.log("top= ",windowScroll);
-    //     console.log("hauteur fenêtre= ",windowHeight);
-    //     console.log("position élément= ",posY);
-    //     console.log("hauteur élément= ",posY-bottomElement);
-    //     if (posY < (windowHeight * .7)) {
-        //         $arg.classList.add('fadeDebut');
-        //     }
-        //     else {
-            //         $arg.classList.remove($anim);
-            //     }
-            // }
             
-var s = skrollr.init();
-            
+let s = skrollr.init();
+
+// Debug Skrollr - suppression style height balise body
+const tagBody= document.querySelector("body");
+tagBody.style.height = null;
+
+// // Déclaration des variables
 let sectionBanner = document.querySelector(".banner");
 let divBanner = document.querySelector(".bannerDiv");
 let sectionStory = document.querySelector("#story");
@@ -59,40 +13,72 @@ let divStory = document.querySelector(".storyDiv");
 let sectionStudio = document.querySelector("#studio");
 let divStudio = document.querySelector(".studioDiv");
 
-function fadeinAnim() {
-    let bannerRect = sectionBanner.getBoundingClientRect();
-    let bannerY = bannerRect.top;
-    let storyRect = sectionStory.getBoundingClientRect();
-    let storyY = storyRect.top;
-    let studioRect = sectionStudio.getBoundingClientRect();
-    let studioY = studioRect.top;
-    let windowHeight = window.innerHeight;
-    // let windowScroll = window.scrollY;
-    console.log("position élément= ",storyY);
-    console.log("hauteur fenêtre= ",windowHeight);
-    // console.log("top= ",windowScroll);
-    if (bannerY < (windowHeight * .85)) {
-        divBanner.classList.add('fadeDebutDown');
-    }
-    else {
-        sectionBanner.classList.remove('fadeDebutDown');
-    }
-    if (storyY < (windowHeight * .85)) {
-        divStory.classList.add('animDebut');
-    }
-    else {
-        divStory.classList.remove('animDebut');
-    }
-    if (studioY < (windowHeight * .85)) {
-        divStudio.classList.add('animDebut');
-    }
-    else {
-        divStudio.classList.remove('animDebut');
+let spanTitleA = document.querySelector(".title_A");
+let spanTitleB = document.querySelector(".title_B");
+let spanStoryTitle = document.querySelector(".storyTitle");
+
+function isElementVisible(Element) {
+    const rect = Element.getBoundingClientRect();
+    const y = rect.top;
+    const windowHeight = window.innerHeight;
+    
+    return y < (windowHeight * 0.85);
+}
+
+function animateIfVisible(Element, divElement, className) {
+    if (isElementVisible(Element)) {
+        divElement.classList.add(className);
+    } else {
+        divElement.classList.remove(className);
     }
 }
 
-// window.addEventListener('load',fadeinAnimOK);
-// window.addEventListener('resize',fadeinAnimOK);
-// window.addEventListener('scroll',fadeinAnimOK);
+// Utilisation de la fonction de test et de la fonction d'animation
+function fadeinAnim() {
+    animateIfVisible(sectionBanner, divBanner, 'fadeDebutDown');
+    animateIfVisible(sectionStory, divStory, 'animDebut');
+    animateIfVisible(sectionStory, spanStoryTitle, 'animStoryTitle');
+    animateIfVisible(sectionStudio, divStudio, 'animDebut');
+    animateIfVisible(sectionStudio, divStudio, 'animDebut');
+    animateIfVisible(sectionStudio, spanTitleA, 'animTitle_A');
+    animateIfVisible(sectionStudio, spanTitleB, 'animTitle_B');
+}
+
 ['load','resize','scroll'].forEach(events => window.addEventListener(events, fadeinAnim));
 
+
+// function fadeinAnim() {
+//     let bannerRect = sectionBanner.getBoundingClientRect();
+//     let bannerY = bannerRect.top;
+//     let storyRect = sectionStory.getBoundingClientRect();
+//     let storyY = storyRect.top;
+//     let studioRect = sectionStudio.getBoundingClientRect();
+//     let studioY = studioRect.top;
+//     let windowHeight = window.innerHeight;
+//     // let windowScroll = window.scrollY;
+//     // console.log("position élément= ",storyY);
+//     // console.log("hauteur fenêtre= ",windowHeight);
+//     // console.log("top= ",windowScroll);
+//     if (bannerY < (windowHeight * .85)) {
+//         divBanner.classList.add('fadeDebutDown');
+//     }
+//     else {
+//         sectionBanner.classList.remove('fadeDebutDown');
+//     }
+//     if (storyY < (windowHeight * .85)) {
+//         divStory.classList.add('animDebut');
+//     }
+//     else {
+//         divStory.classList.remove('animDebut');
+//     }
+//     if (studioY < (windowHeight * .85)) {
+//         divStudio.classList.add('animDebut');
+//     }
+//     else {
+//         divStudio.classList.remove('animDebut');
+//     }
+// }
+
+// // window.addEventListener('load',fadeinAnimOK);
+// // window.addEventListener('resize',fadeinAnimOK);
+// // window.addEventListener('scroll',fadeinAnimOK);
